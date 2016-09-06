9c4bc74d31c3ebe303f7bd1809ae288321fbf85a
Bryan Carlson <bryan.r.carlson@gmail.com>
2016-09-06 14:41:41 -0700

    Added meeting minutes, restyled, updated content

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Sets clicked nav item to active
+$(".nav li").on("click", function() {
+      $(".nav li").removeClass("active");
+      $(this).addClass("active");
+});
+
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 400);
+});
